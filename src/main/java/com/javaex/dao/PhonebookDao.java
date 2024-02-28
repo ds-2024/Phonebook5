72a8afb8af572c4cbc439ead9dd4e53eb3bab49f
ds-2024 <yoonds714@nate.com>
2024-02-28 18:10:51 +0900

    Spring D-3 by MyBatis

--- src/main/java/com/javaex/dao/PhonebookDao.java
+++ src/main/java/com/javaex/dao/PhonebookDao.java
@@ -1,12 +1,6 @@
 package com.javaex.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
-
-import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,57 +10,57 @@ import com.javaex.vo.PersonVo;
 
 @Repository
 public class PhonebookDao {
-	
+
 	@Autowired
 	private SqlSession sqlSession;
-	
-	//삭제
+
+	// 삭제
 	public int personDelete(int no) {
 		System.out.println("PhonebookDao.personDelete()");
-		
+
 		int count = sqlSession.delete("phonebook.delete", no);
 		System.out.println(count);
 		return count;
-	
+
 	}
-	
+
 	// 수정
 	public int personUpdate(PersonVo personVo) {
 		System.out.println("PhonebookDao.personUpdate()");
-		
+
 		int count = sqlSession.update("phonebook.update", personVo);
 		System.out.println(count);
 		System.out.println(personVo);
 		return count;
 	}
-	
-	//수정폼(1개 데이터 가져오기 뿌릴데이터 가져오기)
-	//1개 가져오기
+
+	// 수정폼(1개 데이터 가져오기 뿌릴데이터 가져오기)
+	// 1개 가져오기
 	public PersonVo personSelectOne(int no) {
 		System.out.println("PhonebookDao.personSelectOne()");
-		
+
 		PersonVo personVo = sqlSession.selectOne("phonebook.selectOne", no);
-		
+
 		System.out.println(personVo);
-		
+
 		return personVo;
 	}
-	//등록
+
+	// 등록
 	public int personInsert(PersonVo personVo) {
 		System.out.println("PhonebookDao.personInsert()");
-		
+
 		int count = sqlSession.insert("phonebook.insert", personVo);
-		
+
 		return count;
 	}
 
-	
 	// 전체가져오기
-		public List<PersonVo> personSelect() {
-			System.out.println("phonebookService.exeList()");
-			List<PersonVo> personList = sqlSession.selectList("phonebook.select");
-			System.out.println(personList);
-			return personList;
-		}
-	
+	public List<PersonVo> personSelect() {
+		System.out.println("phonebookService.exeList()");
+		List<PersonVo> personList = sqlSession.selectList("phonebook.select");
+		System.out.println(personList);
+		return personList;
+	}
+
 }
--- src/main/java/com/javaex/service/PhonebookService.java
+++ src/main/java/com/javaex/service/PhonebookService.java
@@ -10,66 +10,61 @@ import com.javaex.vo.PersonVo;
 
 @Service
 public class PhonebookService {
-	
-	//필드
+
+	// 필드
 	@Autowired
-	private PhonebookDao phonebookDao; //Dao 자동 연결
-	//생성자
-	//메소드 gs
-	//메소드 일반
-	
-	//수정
-		public int exeModify(PersonVo personVo) {
-			System.out.println("PhonebookService.exeModify()");
-			
-			
-			int count = phonebookDao.personUpdate(personVo);
-			System.out.println(personVo);
-			return count;
-			
-		}
-	
-	//수정폼
+	private PhonebookDao phonebookDao; // Dao 자동 연결
+	// 생성자
+	// 메소드 gs
+	// 메소드 일반
+
+	// 수정
+	public int exeModify(PersonVo personVo) {
+		System.out.println("PhonebookService.exeModify()");
+
+		int count = phonebookDao.personUpdate(personVo);
+		System.out.println(personVo);
+		return count;
+
+	}
+
+	// 수정폼
 	public PersonVo exeModifyForm(int no) {
 		System.out.println("PhonebookService.exeModifyForm()");
-		//PhonebookDao phonebookDao = new PhonebookDao();
-		
+		// PhonebookDao phonebookDao = new PhonebookDao();
+
 		PersonVo personVo = phonebookDao.personSelectOne(no);
-		
-		
+
 		return personVo;
 	}
-	
-	//등록
-		public void exeWrite(PersonVo personVo) {
-			System.out.println("PhonebookService.exeWrite()");
-			
-			//비즈니스로직 내용들어가야하는 자리
-			PhonebookDao phonebookDao = new PhonebookDao();
-			
-			phonebookDao.personInsert(personVo);
-		}
-	
-	
-	//리스트
+
+	// 등록
+	public void exeWrite(PersonVo personVo) {
+		System.out.println("PhonebookService.exeWrite()");
+
+		// 비즈니스로직 내용들어가야하는 자리
+		PhonebookDao phonebookDao = new PhonebookDao();
+
+		phonebookDao.personInsert(personVo);
+	}
+
+	// 리스트
 	public List<PersonVo> exeList() {
-		//PhonebookDao phonebookDao = new PhonebookDao();
+		// PhonebookDao phonebookDao = new PhonebookDao();
 		List<PersonVo> personList = phonebookDao.personSelect();
-		
+
 		return personList;
 	}
-	
-	//삭제
+
+	// 삭제
 	public int exeDelete(int no) {
 		System.out.println("PhonebookService.exeDelete()");
-		
-		//PhonebookDao phonebookDao = new PhonebookDao();
-		
+
+		// PhonebookDao phonebookDao = new PhonebookDao();
+
 		int count = phonebookDao.personDelete(no);
-		
+
 		return count;
 	}
-	
-	
 
 }
